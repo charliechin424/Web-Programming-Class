94e0298cf6d0d4f3933f24823034a2b17de5731a
Pei-chieh Chin <charlie920424@gmail.com>
2022-10-18 12:32:58 +0800

    hack1

---
+++ hack1/src/components/Board.js
@@ -0,0 +1,24 @@
+/****************************************************************************
+  FileName      [ Board.js ]
+  PackageName   [ src/components ]
+  Author        [ Cheng-Hua Lu ]
+  Synopsis      [ This file generates the Board. ]
+  Copyright     [ 2022 10 ]
+****************************************************************************/
+
+import Row from "./Row";
+import './css/Board.css';
+import React from "react";
+import CurRow from "./CurRow";
+
+const Board = ({ turn, guesses, curGuess }) => {
+    return (
+        <div className="Board-container">
+            {guesses.map((guess, index) => {return index === turn ? <Row id={index} key={index} rowIdx={turn} guess={guess}/> : <CurRow id={index} key={index} rowIdx={index} curGuess={curGuess}/>})}
+            {/* TODO 2-2: show 6 rows (map function is recommended) and defined row's key.
+                Hint: Use `CurRow` instead of `Row` when you are passing `curGuess` into it. */}
+            
+        </div>
+    )
+};
+export default Board;
---
+++ hack1/src/components/Row.js
@@ -0,0 +1,32 @@
+/****************************************************************************
+  FileName      [ Row.js ]
+  PackageName   [ src/components ]
+  Author        [ Cheng-Hua Lu ]
+  Synopsis      [ This file generates the Row. ]
+  Copyright     [ 2022 10 ]
+****************************************************************************/
+
+import "./css/Row.css";
+import React from 'react';
+
+
+const Row = ({ guess, rowIdx }) => {
+    return (
+        <div className='Row-container'>
+            {/* TODO 3: Row Implementation -- Row */}
+            
+            {/* ↓ Default row, you should modify it. ↓ */}
+            <div className='Row-wrapper'>
+                {/* {guess.map((wordbox, index) => {return (<div id={rowIdx + '-' + index} key={rowIdx + '-' + index} className='Row-wordbox'>{wordbox}</div>)})} */}
+                <div id={rowIdx + '0'} key={rowIdx + '0'} className='Row-wordbox '>{guess}</div>
+                <div id={rowIdx + '1'} key={rowIdx + '1'} className='Row-wordbox '>{guess}</div>
+                <div id={rowIdx + '2'} key={rowIdx + '2'} className='Row-wordbox '>{guess}</div>
+                <div id={rowIdx + '3'} key={rowIdx + '3'} className='Row-wordbox '>{guess}</div>
+                <div id={rowIdx + '4'} key={rowIdx + '4'} className='Row-wordbox '>{guess}</div>
+            </div>
+            {/* ↑ Default row, you should modify it. ↑ */}
+        </div>
+    )
+}
+
+export default Row;
